099cb4cefd04ee687789693be8a280bcaacd266d
Hugo Luna <hugo.luna@webpoint.mx>
2022-02-15 19:46:16 -0600

    Added suppor to custom time zone

--- utils/messages.js
+++ utils/messages.js
@@ -1,10 +1,10 @@
-const moment = require('moment');
+var moment = require('moment-timezone');
 
 function formatMessage(username, text) {
   return {
     username,
     text,
-    time: moment().format('h:mm a')
+    time: moment().tz('America/Mexico_City').format('h:mm a'),
   };
 }
 
